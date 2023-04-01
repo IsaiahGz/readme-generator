989336faca40d5fd776ab1829cd8e9c752bd3356
IsaiahGz <isaiahgala99@gmail.com>
2023-04-01 12:46:14 -0400

    Add questions

--- index.js
+++ index.js
@@ -2,14 +2,66 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions array to pass to inquirer
+const questions = [
+	{
+		type: 'input',
+		name: 'username',
+		message: 'What is your GitHub username?',
+	},
+	{
+		type: 'input',
+		name: 'email',
+		message: 'What is your email address?',
+	},
+	{
+		type: 'input',
+		name: 'projectName',
+		message: "What is your project's name?",
+	},
+	{
+		type: 'input',
+		name: 'projectDescription',
+		message: 'Write a short description for your project:',
+	},
+	{
+		type: 'list',
+		name: 'license',
+		message: 'Which license do you want your project to use?',
+		choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+	},
+	{
+		type: 'input',
+		name: 'commandRun',
+		message: 'What command should be run to install dependencies?',
+		default: 'npm i',
+	},
+	{
+		type: 'input',
+		name: 'commandTest',
+		message: 'What command should be run to execute tests?',
+		default: 'npm run test',
+	},
+	{
+		type: 'input',
+		name: 'useRepo',
+		message: 'What does a user need to know about using the repository?',
+	},
+	{
+		type: 'input',
+		name: 'contributeRepo',
+		message: 'What does a user need to know about contributing to the repository?',
+	},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Starter function
+async function init() {
+	const response = await inquirer.prompt(questions);
+	console.log(response);
+}
 
 // Function call to initialize app
 init();
