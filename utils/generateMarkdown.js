a5b51fe8b45ab1a20a99d8789a35a4328f56cd67
IsaiahGz <isaiahgala99@gmail.com>
2023-04-03 18:30:53 -0400

    Render license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -19,10 +19,31 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+	switch (license) {
+		case 'MIT':
+			return 'https://opensource.org/licenses/MIT';
+		case 'Apache 2.0':
+			return 'https://opensource.org/licenses/Apache-2.0';
+		case 'GNU GPL v3':
+			return 'https://www.gnu.org/licenses/gpl-3.0';
+		case 'BSD 3':
+			return 'https://opensource.org/licenses/BSD-3-Clause';
+		case 'ISC':
+			return 'https://opensource.org/licenses/ISC';
+		case '':
+			return '';
+	}
+}
 
 // Outputs the string to place in the license sections
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+	if (!license) {
+		return 'This project is not under any license.';
+	} else {
+		return `This project is under the [${license}](${renderLicenseLink(license)}) license`;
+	}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
